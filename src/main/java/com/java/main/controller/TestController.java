09faecc8d85596f210c900befbd0c8d5b3c8e017
jinkyu <wlsrb8993@gmail.com>
2021-03-21 01:46:32 +0900

    최초 등록

---
+++ src/main/java/com/java/main/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.java.main.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+
+@RestController
+public class TestController {
+	
+	@RequestMapping("/test")
+	public Map<String, Object> test(){
+		
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		
+		resultMap.put("result", "testOK");
+		
+		return resultMap;
+	}
+
+}
