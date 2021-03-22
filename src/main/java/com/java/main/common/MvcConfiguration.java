7d1f140a9680bb77c30acfd4f9593fabc2dfa5e8
jinkyu <wlsrb8993@gmail.com>
2021-03-23 02:27:37 +0900

    로깅 인터셉터 정의.

---
+++ src/main/java/com/java/main/common/MvcConfiguration.java
@@ -0,0 +1,17 @@
+package com.java.main.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.java.interceptor.LoggerInterceptor;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer  {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LoggerInterceptor())
+		.excludePathPatterns("/mapper/**");
+	}
+}
