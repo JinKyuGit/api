aa3a264e9faff01d193bd2ee979cf0714cfcce26
jinkyu <wlsrb8993@gmail.com>
2021-04-04 18:34:49 +0900

    캐릭터 bo 생성.

--- src/main/java/com/java/main/bo/CommonBo.java
+++ src/main/java/com/java/main/bo/CommonBo.java
@@ -3,5 +3,17 @@ package com.java.main.bo;
 import java.io.Serializable;
 
 public class CommonBo implements Serializable {
+	
+	protected String sysDate;
+
+	public String getSysDate() {
+		return sysDate;
+	}
+
+	public void setSysDate(String sysDate) {
+		this.sysDate = sysDate;
+	}
+	
+	
 
 }
---
+++ src/main/java/com/java/main/bo/characterBo.java
@@ -0,0 +1,33 @@
+package com.java.main.bo;
+
+public class characterBo {
+	
+	private String characterName;
+	private String characterAlias;
+	private String location;
+	
+	
+	public String getCharacterName() {
+		return characterName;
+	}
+	public void setCharacterName(String characterName) {
+		this.characterName = characterName;
+	}
+	public String getCharacterAlias() {
+		return characterAlias;
+	}
+	public void setCharacterAlias(String characterAlias) {
+		this.characterAlias = characterAlias;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	
+	
+	
+
+}
