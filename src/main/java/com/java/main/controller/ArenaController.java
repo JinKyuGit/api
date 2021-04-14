b84c5f15d705cb37ea3edd90bba20e3ebb7df4b6
jinkyu <wlsrb8993@gmail.com>
2021-04-14 16:29:34 +0900

    아레나 등록 - 트랜잭션 처리

--- src/main/java/com/java/main/controller/ArenaController.java
+++ src/main/java/com/java/main/controller/ArenaController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +36,7 @@ public class ArenaController {
 		return resultMap;
 	}
 	
+	@Transactional
 	@RequestMapping("/registerArena")
 	public Map<String, Object> registerArena(@RequestBody ArenaInfoBo param) throws Exception{
 		
